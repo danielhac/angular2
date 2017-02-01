89142e0ee0cfae81ad59c5846540f0e4d22eb1a4
Dan <danhboi23@hotmail.com>
2017-02-01 11:49:14 -0800

    Testing @Output - send data from child to Parent component

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
@@ -13,11 +13,18 @@ import { Component, Input } from '@angular/core'
                 <span>&nbsp;</span>
                 <span>{{event.location.city}}, {{event.location.country}}</span>
             </div>
+            <button class="btn btn-primary" (click)="handleClickMe()">Test</button>
         </div>
     `
 })
 export class EventThumbnailComponent {
     // Public prop in typescript that allows any datatype for event
-    // Input: tells Ang that this event will be passed from another comp
+    // Input: tells Ang that this event will be passed from another comp (Parent to child)
     @Input() event:any
+    // Output: Child to parent
+    @Output() eventClick = new EventEmitter()
+
+    handleClickMe() {
+        this.eventClick.emit(this.event.name)
+    }
 }
