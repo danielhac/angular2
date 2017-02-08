18d7a0f3519aad959af596726c18301db9c0555f
Dan <danhboi23@hotmail.com>
2017-02-07 16:20:19 -0800

    Added ngFor to repeat data and added styling

--- app/events/event-thumbnail.component.js
+++ app/events/event-thumbnail.component.js
@@ -21,7 +21,7 @@ var EventThumbnailComponent = (function () {
         core_1.Component({
             selector: 'event-thumbnail',
             template: "\n        <div class=\"well hoverwell thumbnail\">\n            <h2>{{event.name}}</h2>\n            <div>Date: {{event.date}}</div>\n            <div>Time: {{event.time}}</div>\n            <div>Price: {{event.price}}</div>\n            <div>\n                <span>Location: {{event.location.address}}</span>\n                <span class=\"pad-left\">{{event.location.city}}, {{event.location.country}}</span>\n            </div>\n        </div>\n    ",
-            styles: ["\n        .pad-left { margin-left: 5px; }\n        .well div { color: #2f96b4; }\n    "]
+            styles: ["\n        .thumbnail { min-height: 210px; }\n        .pad-left { margin-left: 5px; }\n        .well div { color: #2f96b4; }\n    "]
         }), 
         __metadata('design:paramtypes', [])
     ], EventThumbnailComponent);
